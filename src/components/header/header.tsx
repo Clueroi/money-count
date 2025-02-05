ef06264b18e11b94e52c66518f698888d5777723
clueroi <eriicsouzaromeroo@gmail.com>
2025-02-05 19:21:22 -0300

    Feat: Modal with Radix

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,14 +1,27 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
 import logo from '../../assets/logo.svg'
+import * as Dialog from "@radix-ui/react-dialog";
 
-export function Header(){
+export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logo} alt="" />
 
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>Nova Transação</Dialog.Title>
+              <Dialog.Close>Close</Dialog.Close>
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
