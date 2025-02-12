320ea5289a0ece71a3fdb5a9b90e85526a1966dc
clueroi <eriicsouzaromeroo@gmail.com>
2025-02-11 22:59:28 -0300

    Feat: Functional SearchBar

--- src/pages/transactions/searchForm/index.tsx
+++ src/pages/transactions/searchForm/index.tsx
@@ -3,6 +3,8 @@ import { SearchFormContainer } from "./styles";
 import { useForm } from "react-hook-form";
 import * as z from 'zod'
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useContext } from "react";
+import { TransactionsContext } from "../../../contexts/transactionsContext";
 
 const searchFormSchema = z.object({
   query: z.string()
@@ -12,12 +14,14 @@ type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
 
+  const { fetchTransactions} = useContext(TransactionsContext)
+
   const { register, handleSubmit, formState: {isSubmitting}} = useForm<SearchFormInputs>({
     resolver: zodResolver(searchFormSchema)
   })
 
-  function handleSearchTransaction(data: SearchFormInputs) {
-    console.log(data)
+  async function handleSearchTransaction(data: SearchFormInputs) {
+    await fetchTransactions(data.query)
   }
 
   return (
